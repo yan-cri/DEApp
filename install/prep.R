9cb864bd3b0a04e84f8e9afe538b499ea804432c
yli22@bsd.uchicago.edu <yli22@bsd.uchicago.edu>
2015-07-20 11:51:16 -0500

    add prep.R inside install folder

---
+++ install/prep.R
@@ -0,0 +1,14 @@
+packages <- c("shinydashboard", "DT","shiny", "ggplot2")
+if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
+  install.packages(setdiff(packages, rownames(installed.packages())))  
+}
+
+BCpackages <- c("edgeR", "DESeq2", "limma")
+if (length(setdiff(BCpackages, rownames(installed.packages()))) > 0) {
+  source("http://bioconductor.org/biocLite.R")
+  biocLite(setdiff(BCpackages, rownames(installed.packages())))
+}
+
+sapply(c(packages, BCpackages), require, character.only=T)
+
+print(sapply(c(packages, BCpackages), require, character.only=T))
